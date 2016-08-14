f8dd2b50a96eeb6f618d35570bc5e7f12679554c
honza <honza@DESKTOP-7R7I741>
2016-08-14 22:11:32 +0200

    Pridani podminky pro predmet emailu

--- demo.php
+++ demo.php
@@ -11,8 +11,10 @@
         $imap = new imap('{imap.mujserver.cz}', 'muj@email.cz', 'mojeHesloKEmailu');
         $messages = $imap->emailsToParse();
         foreach(array_reverse($messages) as $message){
-            $message = bank::csob($message['message']);
-            //zde by pak bylo na miste ulozit data do db
+            if($message['header'] == 'ČSOB Info 24 - Avízo'){
+                $message = bank::csob($message['message']);
+                //zde by pak bylo na miste ulozit data do db
+            }
         }
         ?>
     </body>
